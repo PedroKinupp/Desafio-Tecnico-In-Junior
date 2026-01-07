cfb75d397aa91ea31e496efc277061c511460e71
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-07 14:09:36 -0300

    feat(questão 2): add  pedindo ao usuario um numero

--- Questao_2.js
+++ Questao_2.js
@@ -1,9 +1,12 @@
 function fatorial(number){
-    if(number<0 || number%1!=0) return "número inválido"
+    if(number<0 || number%1!=0) return "número inválido";
     let resultado=1;
     while(number>1){
         resultado*=number;
         number--;
     }
-    return "fatorial: " + resultado
+    return "fatorial: " + resultado;
 }
+
+number=prompt("digite um número para calcular seu fatorial");
+console.log(fatorial(number));

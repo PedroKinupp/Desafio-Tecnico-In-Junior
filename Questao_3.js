2ffbc0c23a2aa725fbf2de340be5f2280bc226a4
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-07 14:56:47 -0300

    test(questão 3): add teste

--- Questao_3.js
+++ Questao_3.js
@@ -9,3 +9,9 @@ function verificarIdades(array, ano){
     });
     return maioridade
 }
+
+
+let array = [2006,2010,2012,1971,1998]
+let ano = prompt("Digite o ano")
+let idades = verificarIdades(array,ano)
+console.log(idades)

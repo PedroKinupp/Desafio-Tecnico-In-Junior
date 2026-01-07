8656aa52237e1bd4dfd32387f3cb8f21ed3749b6
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-07 14:06:41 -0300

    feat(questão 2): add verificação de validade do numero inserido

--- Questao_2.js
+++ Questao_2.js
@@ -1,8 +1,9 @@
 function fatorial(number){
+    if(number<0 || number%1!=0) return "número inválido"
     let resultado=1;
     while(number>1){
         resultado*=number;
         number--;
     }
-    return resultado
+    return "fatorial: " + resultado
 }

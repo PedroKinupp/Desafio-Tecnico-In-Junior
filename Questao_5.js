80c88b72620ed62df772b6942b49a90a397dd013
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-07 15:30:43 -0300

    refactor(questão 5): modificação no print da mensagem

--- Questao_5.js
+++ Questao_5.js
@@ -2,4 +2,5 @@ let number = prompt("Digite um número inteiro");
 let mensagem = ""
 if(number%3==0) mensagem += "fizz"
 if(number%5==0) mensagem += "buzz"
-console.log(mensagem)
+
+if(mensagem!= "") console.log(mensagem) 

769837407d4e3b1ad91f212fed8f09914d506798
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-07 01:15:49 -0300

    feat: resolução do primeiro exercício

--- Questao_1.js
+++ Questao_1.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+let a = prompt("digite o coeficiente a");
+let b = prompt("digite o coeficiente b");
+let c = prompt("digite o coeficiente c");
+let delta = (b*b) + (-4*a*c);
+console.log("delta: " + delta);
+
+if(delta<0){
+    console.log("equação não possui raízes")
+}else if(delta==0){
+    console.log("equação possui uma raíz")
+    let raiz = (-b)/(2*a);
+    console.log("raiz: " + raiz);
+}else{
+    console.log("equação possui duas raízes")
+    let raiz1=((-b) + Math.sqrt(delta))/(2*a);
+    let raiz2=((-b) - Math.sqrt(delta))/(2*a);
+    console.log("raizes: " + raiz1 + ", " + raiz2);
+}

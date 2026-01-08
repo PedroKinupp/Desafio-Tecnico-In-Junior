aa0c3e2262360e6ccc75bcfb71e4bc3de855081f
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-08 19:09:13 -0300

    feat(questão 7): verificando se é palíndromo

--- Questao_7.js
+++ Questao_7.js
@@ -8,4 +8,14 @@ frase.forEach((element, indice, frase) => {
     }
 })
 
-console.log(frase)
+//verrificando se é palíndromo
+let resp = "É palíndromo"
+let length = frase.length
+for(let i=0 ; i < Math.floor(length/2) ; i++){
+    if(frase[i] != frase[-i+length-1]){
+        resp = "Não é palíndromo"
+        break
+    }
+}
+
+console.log(resp)

d56aa03a339c3f22c490d6e4f8f15981bf225979
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-08 19:35:51 -0300

    test(quastão 8): add teste com array de objetos similar ao do exercício

--- Questao_8.js
+++ Questao_8.js
@@ -1,11 +1,92 @@
 function countBooksInCategory(array){
     const object = {}
 
-    array.forEach((element, indice) =>{
-        let counter = element.books.length()
-        let key = element.category
-        object.key = counter
+    array.forEach((element) =>{
+        let counter = element.books.length
+        object[element.category] = counter
     })
 
     return object
 }
+
+const booksByCategory =[
+    {
+        category: "Romance",
+        books: [
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+        ]
+    },
+    {
+        category: "Fantasia",
+        books: [
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+        ]
+    },
+    {
+        category: "Suspense",
+        books: [
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+        ]
+    },
+    {
+        category: "Ficção Científica",
+        books: [
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+            {
+                titulo: "seila",
+                autor: "seila também"
+            },
+        ]
+    },
+]
+
+const objetofinal = countBooksInCategory(booksByCategory)
+console.log(objetofinal)

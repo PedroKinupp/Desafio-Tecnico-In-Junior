a793582c81cc244cad023f99dc3db377dd5f91e9
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-09 16:30:11 -0300

    feat(questão 12): add função que encontra o elemento único

---
+++ Questao_12.js
@@ -0,0 +1,9 @@
+function encontrarElementoUnico(array){
+    let aux = [];
+    array.forEach(element => {
+        let pos = aux.indexOf(element); //posição do elemento no vetor auxiliar
+        if(pos == -1) aux.push(element)
+        else aux.splice(pos,1)
+    })
+    return aux[0]
+}

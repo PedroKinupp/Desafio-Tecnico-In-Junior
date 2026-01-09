884502120eec87413b4bc78ea071b5d372baeb8f
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-01-08 21:23:59 -0300

    feat(questão 9): função não coloca mais nomes repetidos

--- Questao_9.js
+++ Questao_9.js
@@ -2,7 +2,7 @@ function authors(array){
     let autores = []
     array.forEach(categorias => {
         categorias.books.forEach(livros =>{
-            autores.push(livros.autor)
+            if(autores.indexOf(livros.autor) == -1) autores.push(livros.autor)
         })
     });
     return autores
